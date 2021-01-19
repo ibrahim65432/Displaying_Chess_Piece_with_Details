12f059b4fd66ee2cf0a798abda49503e5e5eb2b1
Ibrahim65432 <kiva732845@>
2021-01-18 22:34:27 -0500

    Initial commit

---
+++ app/src/main/java/com/edureka/training/fragmentsvideo2021/DetailFrag.java
@@ -0,0 +1,27 @@
+package com.edureka.training.fragmentsvideo2021;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DetailFrag#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DetailFrag extends Fragment {
+    public DetailFrag () {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
